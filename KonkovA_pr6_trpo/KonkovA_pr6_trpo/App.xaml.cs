3a01b7181140ee5f81d548702235f05ea034c620
Ghosterenk0 <arsenijkonkov912@gmail.com>
2025-10-31 11:48:47 +0500

    Add files via upload

---
+++ KonkovA_pr6_trpo/KonkovA_pr6_trpo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KonkovA_pr6_trpo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
